12790ecc4380bf39543285d95bcbb3f00a41577f
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-10-13 17:36:07 +0300

    feat: add dream-team impl

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  const teamName = members.map((item, i, arr) => {
+    if(typeof(item) == 'string') {
+      return item = item.trim().toUpperCase().split('')[0];
+    }
+  });
+
+  teamName.sort();
+
+  
+  return teamName.join('');
+
 }
 
 module.exports = {
