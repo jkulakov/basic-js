9756ec8bd94ad45eb10aeb4b1f8e363054ab3208
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-10-13 19:14:00 +0300

    feat: add encode-line impl

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  str = str.split('');
+  let strCounter = []; strCounter.push(str[0])
+  let finalStr = '';
+
+  for (let i = 1; i <= str.length; i++) {
+    if(str[i] == strCounter[strCounter.length-1]) {
+
+      strCounter.push(str[i]);
+    } else {
+      console.log(strCounter)
+      let c = strCounter.length; if(strCounter.length == 1) c = '';
+      finalStr += c + strCounter[0];
+      strCounter = [];
+      strCounter.push(str[i]);
+    }
+  }
+  return finalStr; 
 }
+
+// console.log(encodeLine('abbcca'));
 
 module.exports = {
   encodeLine
