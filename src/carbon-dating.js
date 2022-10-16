5b03dfcad8b4ff49397f682f669fd453734199f9
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-10-16 19:44:44 +0300

    feat: add carbon dating impl

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  if(!sampleActivity) { 
+    return false; 
+  } else if(sampleActivity > 15 || sampleActivity <= 0) {
+    return false;
+  } else if(Number.isNaN(Number(sampleActivity))) {
+    return false;
+  } else if(typeof(sampleActivity) !== 'string') {
+    return false;
+  }
+
+  const firstO = Math.log(MODERN_ACTIVITY / sampleActivity);
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(firstO / k);
+
 }
 
 module.exports = {
