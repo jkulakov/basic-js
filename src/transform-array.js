1444b9b72f8f076b3a95a7b09ba98ae4cb94488e
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-10-17 18:58:11 +0300

    fear: add transform array impl

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let resultArray = [];
+
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    resultArray.push(arr[i]);
+
+    if(arr[i] == '--discard-next') {
+        resultArray.pop();
+        if (i < arr.length - 1) i += 2;
+    } else if(arr[i] == '--double-prev') {    
+      resultArray.pop();
+      if(i > 0) {
+      resultArray.push(resultArray[i-1]);
+      }
+    } else if(arr[i] == '--double-next') {    
+      resultArray.pop();
+      if(i < arr.length - 1) {
+          resultArray.push(arr[i+1]);
+      }
+    } else if(arr[i] == '--discard-prev') {    
+      resultArray.pop();
+      if (i > 0) {
+        resultArray.pop();
+      }
+    }
+    
+  }
+  return resultArray;
 }
 
 module.exports = {
